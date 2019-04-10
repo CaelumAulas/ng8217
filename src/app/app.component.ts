23c8c4a7c791608cf569b119f953c1f2801388de
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-04-09 22:54:53 -0300

    Noite 02 - Envio de emails, template drive validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Email } from './models/email';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng8217';
+  readonly patternEmail = "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+  email = new Email();
+  emailList: Email[] = [];
+
+  private _isNewEmailOpen = false;
+
+  get isNewEmailOpen(){
+    return this._isNewEmailOpen;
+  }
+
+  toggleNewEmailForm(){
+    this._isNewEmailOpen = !this.isNewEmailOpen;
+  }
+
+  handleNewEmail(formEmail: NgForm){
+    this.emailList.push(this.email)
+    this.email = new Email();
+    formEmail.resetForm();
+    this.toggleNewEmailForm();
+
+  }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

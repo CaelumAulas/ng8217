8953f645b52373b2f3f1e66382c201758fb4d95b
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-04-09 22:59:47 -0300

    MenuMobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,4 +10,10 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
 
+    isMenuOpen = false;
+
+    toggleMenu(){
+        this.isMenuOpen = !this.isMenuOpen;
+    }
+
 }
